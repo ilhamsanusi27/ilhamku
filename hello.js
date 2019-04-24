86b65804d3e323bf359c84ec6b230b4a18d1e06b
Ilham Sanusi <34309870+ilhamsanusi27@users.noreply.github.com>
2019-04-24 08:11:29 +0700

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,3 @@
+const button = document.getElementById('hello');
+button.addEventListener('click', () => {
+ const el = document.createElement('h1'); el.textContent = 'Hello World!'; document.body.appendChild(el); });
